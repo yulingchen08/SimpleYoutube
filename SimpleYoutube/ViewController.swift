d27e655c9fa42c4ce333e17e5e906a2cdabf5c4c
Eric Chen <eric.chen@gozeppelin.com>
2023-03-10 22:09:56 +0800

    Initial Commit

---
+++ SimpleYoutube/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleYoutube
+//
+//  Created by 陳鈺翎 on 2023/3/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SimpleYoutubeUITests/SimpleYoutubeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SimpleYoutubeUITestsLaunchTests.swift
+//  SimpleYoutubeUITests
+//
+//  Created by 陳鈺翎 on 2023/3/10.
+//
+
+import XCTest
+
+final class SimpleYoutubeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
